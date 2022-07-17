f32599fdef702b77f16d0e7d01f409cf9cea511a
wusskk <shikaicang@163.com>
2022-07-17 20:21:30 +0800

    move hello-world/ and hello-cargo/ to chapter-1/

---
+++ chapter-1/hello-cargo/target/debug/deps/hello_cargo-909a3e3e5bd3674c.d
@@ -0,0 +1,5 @@
+/home/light/rust-project/hello-cargo/target/debug/deps/hello_cargo-909a3e3e5bd3674c.rmeta: src/main.rs
+
+/home/light/rust-project/hello-cargo/target/debug/deps/hello_cargo-909a3e3e5bd3674c.d: src/main.rs
+
+src/main.rs:
---
+++ chapter-1/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/light/rust-project/hello-cargo/target/debug/hello-cargo: /home/light/rust-project/hello-cargo/src/main.rs
